64c3bba100ff1073f69dcad1475aebce27090db6
Foxy1347 <71808041+Foxy1347@users.noreply.github.com>
2020-10-28 00:46:35 -0500

    Update peerReviewErrors.py

--- peerReviewErrors.py
+++ peerReviewErrors.py
@@ -4,23 +4,25 @@
 # Author: <author>
 # Creation Date: <date>
 # Below is a simple program with 10 issues (some are syntax errors and some are logic errors.  You need to identify the issues and correct them.
+# Shane Fox 10/27/2020 peer review.
 
 import random
 import time
 
 def displayIntro():
-	print('''You are in a land full of dragons. In front of you,
-	you see two caves. In one cave, the dragon is friendly
-	and will share his treasure with you. The other dragon
-	is greedy and hungry, and will eat you on sight.''')
+	print(
+'''You are in a land full of dragons. In front of you,
+you see two caves. In one cave, the dragon is friendly
+and will share his treasure with you. The other dragon
+is greedy and hungry, and will eat you on sight.'''
+)
 	print()
 
 def chooseCave():
-    cave = ''
+	cave = ''
 	while cave != '1' and cave != '2':
 		print('Which cave will you go into? (1 or 2)')
 		cave = input()
-
 	return caves
 
 def checkCave(chosenCave):
@@ -29,7 +31,7 @@ def checkCave(chosenCave):
 	time.sleep(2)
 	print('It is dark and spooky...')
 	#sleep for 2 seconds
-	time.sleep(3)
+	time.sleep(2)
 	print('A large dragon jumps out in front of you! He opens his jaws and...')
 	print()
 	#sleep for 2 seconds
@@ -39,16 +41,16 @@ def checkCave(chosenCave):
 	if chosenCave == str(friendlyCave):
 		print('Gives you his treasure!')
 	else:
-		print 'Gobbles you down in one bite!'
+		print('Gobbles you down in one bite!')
 
 playAgain = 'yes'
-while playAgain = 'yes' or playAgain = 'y':
+while playAgain == 'yes' or playAgain == 'y':
 	displayIntro()
-	caveNumber = choosecave()
+	caveNumber = chooseCave()
 	checkCave(caveNumber)
     
 	print('Do you want to play again? (yes or no)')
 	playAgain = input()
-	if playAgain == "no":
-		print("Thanks for planing")
+	if playAgain == "no" or playAgain == 'n':
+		print("Thanks for playing.")
 
